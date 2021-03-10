29da63b3c43dc59af51f446017202b66d4dcb596
Malcolm <harrismalcolm1@gmail.com>
2021-03-10 14:37:26 -0500

    refactor my function behavior to accept the length of the given arr instead of a predetermined number

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let personality = ['confidant', 'prudent', 'firm']
-let occupation = ['pirate', 'dentist', 'fire-fighter']
-let salary = ['55k', '28k', '100k']
+let personality = ['confidant', 'prudent', 'firm', 'loyal']
+let occupation = ['pirate', 'dentist', 'fire-fighter', 'plumber', 'monster-hunter']
+let salary = ['55k', '28k', '100k', '72k', '250k', '1M']
 
 const careerPredictor = (arr1, arr2, arr3) => {
-    let randomPersonality = arr1[Math.floor(Math.random() * 3)];
-    let randomOccupation = arr2[Math.floor(Math.random() * 3)];
-    let randomSalary = arr3[Math.floor(Math.random() * 3)]
-
+    let randomPersonality = arr1[Math.floor(Math.random() * arr1.length)];
+    let randomOccupation = arr2[Math.floor(Math.random() * arr2.length)];
+    let randomSalary = arr3[Math.floor(Math.random() * arr3.length)]
+    console.log(randomPersonality)
     return `Your fortune tells me you will become a ${randomPersonality} ${randomOccupation} next year. Your starting salary will be $${randomSalary}!`;
 }
 
