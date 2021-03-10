527b34bce694825d8cf3625bb1d3b9ffb474b5a5
Malcolm <harrismalcolm1@gmail.com>
2021-03-10 14:30:43 -0500

    Added randomIndex variable

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ let occupation = ['pirate', 'dentist', 'fire-fighter']
 let salary = ['55k', '28k', '100k']
 
 const careerPredictor = (arr1, arr2, arr3) => {
-    let randomPersonality;
-    let randomOccupation;
-    let randomSalar;
-    console.log(`Your fortune tells me you will become a ${arr1} ${arr2} next year. Your starting salary will be $${arr3}!`);
+    let randomPersonality = arr1[randomIndex];
+    let randomOccupation = arr2[randomIndex];
+    let randomSalary = arr3[randomIndex]
+    let randomIndex = Math.floor(Math.random() * 3)
+    console.log(randomIndex)
+
+    console.log(`Your fortune tells me you will become a ${randomPersonality} ${randomOccupation} next year. Your starting salary will be $${randomSalary}!`);
 }
 
 console.log(careerPredictor(personality, occupation, salary));
