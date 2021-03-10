6b6d126446a38c25296578ee31715b44871cb780
Malcolm <harrismalcolm1@gmail.com>
2021-03-10 14:26:23 -0500

    added careerPredictor function

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-let adverb = ['brave', 'boring', 'aggressive', ]
 let personality = ['confidant', 'prudent', 'firm']
 let occupation = ['pirate', 'dentist', 'fire-fighter']
+let salary = ['55k', '28k', '100k']
+
+const careerPredictor = (arr1, arr2, arr3) => {
+    let randomPersonality;
+    let randomOccupation;
+    let randomSalar;
+    console.log(`Your fortune tells me you will become a ${arr1} ${arr2} next year. Your starting salary will be $${arr3}!`);
+}
+
+console.log(careerPredictor(personality, occupation, salary));
