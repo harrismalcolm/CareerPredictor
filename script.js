f97248fcc995ee375bd76fceed19a7486a56c647
Malcolm <harrismalcolm1@gmail.com>
2021-03-11 15:01:03 -0500

    refactored code to accept no parameters

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ let personality = ['confidant', 'prudent', 'firm', 'loyal']
 let occupation = ['pirate', 'dentist', 'fire-fighter', 'plumber', 'monster-hunter']
 let salary = ['55k', '28k', '100k', '72k', '250k', '1M']
 
-const careerPredictor = (arr1, arr2, arr3) => {
-    let randomPersonality = arr1[Math.floor(Math.random() * arr1.length)];
-    let randomOccupation = arr2[Math.floor(Math.random() * arr2.length)];
-    let randomSalary = arr3[Math.floor(Math.random() * arr3.length)]
+const careerPredictor = () => {
+    let randomPersonality = personality[Math.floor(Math.random() * personality.length)];
+    let randomOccupation = occupation[Math.floor(Math.random() * occupation.length)];
+    let randomSalary = salary[Math.floor(Math.random() * salary.length)]
     return `Your fortune tells me you will become a ${randomPersonality} ${randomOccupation} next year. Your starting salary will be $${randomSalary}!`;
 }
 
-console.log(careerPredictor(personality, occupation, salary));
+console.log(careerPredictory());
